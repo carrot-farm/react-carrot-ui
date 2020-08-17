0504eac972ff2aac8405609eee3ac99df99859be
carrot-farm <chohoki@gmail.com>
2020-08-17 23:05:21 +0900

    feat: Typography 개발 / 모듈 업데이트

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,6 +1,6 @@
-import { css } from '@emotion/core';
-import colors, { mainColors } from './colors';
-import { TMainColorKeys, TColorKeys } from '../types/colors'
+import { css } from "@emotion/core";
+import colors, { mainColors } from "./colors";
+import { TMainColorKeys, TColorKeys } from "../types/colors";
 
 // ===== 타입
 // # 색상 타입을 순회하기 위한 키배열
@@ -8,38 +8,83 @@ export const colorTypes: TColorKeys[] = Object.keys(colors) as any[];
 // # 메인 색상의 배열
 export const mainColorsArr: TMainColorKeys[] = Object.keys(mainColors) as any[];
 // # position Type
-export const positionValues = ["relative", "absolute", "fixed", "static", "sticky"] as const;
+export const positionValues = [
+  "relative",
+  "absolute",
+  "fixed",
+  "static",
+  "sticky",
+] as const;
 export type positionType = typeof positionValues[number];
 // # text align type
 export const textAlignValues = ["left", "center", "right"] as const;
 export type textAlignType = typeof textAlignValues[number];
 // # display type
-export const displayValues = ["inline", "inline-block", "block", "flex", "inline-flex", "none"] as const;
+export const displayValues = [
+  "inline",
+  "inline-block",
+  "block",
+  "flex",
+  "inline-flex",
+  "none",
+] as const;
 export type displayType = typeof displayValues[number];
 // # flext align type
-export const flexAlignValues = ["space-around", "space-between", "flex-start","flex-end", "middle-center", "middle", "center"] as const; 
+export const flexAlignValues = [
+  "space-around",
+  "space-between",
+  "flex-start",
+  "flex-end",
+  "middle-center",
+  "middle",
+  "center",
+] as const;
 export type flexAlignType = typeof flexAlignValues[number];
 // # flex wrap
 export const flexWrapValues = ["wrap", "nowrap"];
 export type flexWrapType = typeof flexWrapValues[number];
 // # flex direction
-export const flexDirectionValues = ["row", "column"] as const
+export const flexDirectionValues = ["row", "column"] as const;
 export type flexDirectionType = typeof flexDirectionValues[number];
 
 // ===== 기본 값
 export const baseUnit = 5;
 
 // ===== margin
-const marginTop = (number: number) => css`margin-top: ${number * baseUnit}px;`;
-const marginRight = (number: number) => css`margin-right: ${number * baseUnit}px;`;
-const marginBottom = (number: number) => css`margin-bottom: ${number * baseUnit}px;`;
-const marginLeft = (number: number) => css`margin-left: ${number * baseUnit}px;`;
+const marginTop = (number: number) =>
+  css`
+    margin-top: ${number * baseUnit}px;
+  `;
+const marginRight = (number: number) =>
+  css`
+    margin-right: ${number * baseUnit}px;
+  `;
+const marginBottom = (number: number) =>
+  css`
+    margin-bottom: ${number * baseUnit}px;
+  `;
+const marginLeft = (number: number) =>
+  css`
+    margin-left: ${number * baseUnit}px;
+  `;
 
 // ===== padding
-const paddingTop = (number: number) => css`padding-top: ${number * baseUnit}px;`;
-const paddingRight = (number: number) => css`padding-right: ${number * baseUnit}px;`;
-const paddingBottom = (number: number) => css`padding-bottom: ${number * baseUnit}px;`;
-const paddingLeft = (number: number) => css`padding-left: ${number * baseUnit}px;`;
+const paddingTop = (number: number) =>
+  css`
+    padding-top: ${number * baseUnit}px;
+  `;
+const paddingRight = (number: number) =>
+  css`
+    padding-right: ${number * baseUnit}px;
+  `;
+const paddingBottom = (number: number) =>
+  css`
+    padding-bottom: ${number * baseUnit}px;
+  `;
+const paddingLeft = (number: number) =>
+  css`
+    padding-left: ${number * baseUnit}px;
+  `;
 
 // ===== border
 const border = () => css`
@@ -62,56 +107,96 @@ const borderLeft = () => css`
   box-sizing: border-box;
   border-left: 1px solid;
 `;
-const borderColor = (color: TColorKeys) => css`border-color: ${colors[color]}`;
+const borderColor = (color: TColorKeys) =>
+  css`
+    border-color: ${colors[color]};
+  `;
 
 // ===== width
-const width = (width: string) => css`width: ${width}`;
+const width = (width: string) =>
+  css`
+    width: ${width};
+  `;
 
 // ===== height
-const height = (height: string) => css`height: ${height}`;
+const height = (height: string) =>
+  css`
+    height: ${height};
+  `;
 
 // ===== background color
-const backgroundColor = (color: TColorKeys) => css`background-color: ${colors[color]}`;
+const backgroundColor = (color: TColorKeys) =>
+  css`
+    background-color: ${colors[color]};
+  `;
 
 // ===== color
-const color = (color: TColorKeys) => css`color: ${colors[color]}`;
-
-
+const color = (color: TColorKeys) =>
+  css`
+    color: ${colors[color]};
+  `;
 
 // ===== position
-const position = (position: positionType) => css`position: ${position}`;
+const position = (position: positionType) =>
+  css`
+    position: ${position};
+  `;
 
 // ===== display
-const display = (display: displayType) => css`display: ${display}`;
+const display = (display: displayType) =>
+  css`
+    display: ${display};
+  `;
 
 // ===== text align
-const textAlign = (align: textAlignType) => css`text-align: ${align}`;
+const textAlign = (align: textAlignType) =>
+  css`
+    text-align: ${align};
+  `;
 
 // ===== flex align
 const flexAlign = (align: flexAlignType) => {
-  if(align === 'center')  {
-    return (css`justify-content: center`) ;
-  } else if(align === 'middle') {
-    return (css`align-items: center`) ;
-  } else if(align === 'middle-center') {
-    return (css`
+  if (align === "center") {
+    return css`
+      justify-content: center;
+    `;
+  } else if (align === "middle") {
+    return css`
+      align-items: center;
+    `;
+  } else if (align === "middle-center") {
+    return css`
       justify-content: center;
       align-items: center;
-    `);
-  } else if(align === 'flex-start') {
-    return css`justify-content: flex-start;`;
-  } else if(align === 'flex-end') {
-    return css`justify-content: flex-end;`;
+    `;
+  } else if (align === "flex-start") {
+    return css`
+      justify-content: flex-start;
+    `;
+  } else if (align === "flex-end") {
+    return css`
+      justify-content: flex-end;
+    `;
   } else if (align === "space-around") {
-    return css`justify-content: space-around;`; 
+    return css`
+      justify-content: space-around;
+    `;
   } else if (align === "space-between") {
-    return css`justify-content: space-between;`; 
+    return css`
+      justify-content: space-between;
+    `;
   }
 };
 // ===== flex wrap
-const flexWrap = (wrap: flexWrapType) => css`flex-wrap: ${wrap}`;
+const flexWrap = (wrap: flexWrapType) =>
+  css`
+    flex-wrap: ${wrap};
+  `;
 // ===== flex wrap
-const flexDirection = (direction: flexDirectionType) => css`flex-direction: ${direction}`;
+const flexDirection = (direction: flexDirectionType) =>
+  css`
+    flex-direction: ${direction};
+  `;
 
 // ===== media query
 export const media: any = {
@@ -122,32 +207,28 @@ export const media: any = {
       }
     `;
   },
-  m: (styles: string) => { 
-    return (
-      css`
-      @media screen and (min-width: 601px){
+  m: (styles: string) => {
+    return css`
+      @media screen and (min-width: 601px) {
         ${styles}
-      }`
-    )
+      }
+    `;
   },
-  mOnly: (styles: string) => { 
-    return (
-      css`
+  mOnly: (styles: string) => {
+    return css`
       @media screen and (min-width: 601px) and (max-width: 960px) {
         ${styles}
-      }`
-    )
+      }
+    `;
   },
   l: (styles: string) => {
-    return (css`
+    return css`
       @media screen and (min-width: 961px) {
         ${styles}
       }
-    `)
+    `;
   },
-}
-
-
+};
 
 // ===== 컬럭의 색상을 반환
 export const getColor = (color: keyof typeof colors) => colors[color];
@@ -171,11 +252,11 @@ export default {
   borderLeft,
   borderColor,
 
-  width, 
+  width,
   height,
 
   color,
-  backgroundColor, 
+  backgroundColor,
 
   position,
   display,
@@ -186,4 +267,4 @@ export default {
 
   media,
   getColor,
-}
+};
